e49012d238ada20fd1088831e9a7d5d95928f26c
Korrado Inganamorte <petristomin1812@gmail.com>
2024-03-24 19:33:27 +0300

    final version of header

--- lib/components/header.dart
+++ lib/components/header.dart
@@ -1,19 +1,62 @@
 import 'package:flutter/material.dart';
 
+import 'package:music/assets_icon_icons.dart';
+
 class Header extends StatelessWidget {
   const Header({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: const Color(0xFF0F0F0F),
-      padding: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-      child: AppBar(
-        title: const Text(
-          "KI",
-        style: TextStyle(fontSize: 28, color: Color(0xFFFFFFFF)),
+    return AppBar(
+      backgroundColor: const Color(0xFF0F0F0F),
+
+      title: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(right: 26.0),
+              child: Text(
+                "KI",
+                style: TextStyle(color: Colors.white, fontSize: 28.0),
+              )
+            ),
+
+            Expanded(
+              child: SizedBox(
+                height: 32,
+                child: TextField(
+                  cursorColor: Colors.white,
+                  cursorHeight: 21.0,
+                  cursorWidth: 1.0,
+
+                  decoration: InputDecoration(
+                    alignLabelWithHint: true,
+
+                    fillColor: const Color(0xFF2B2B2B),
+                    filled: true,
+
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 18.0, vertical: 9.0),
+
+                    border: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+                    enabledBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+                    focusedBorder: OutlineInputBorder(borderRadius: BorderRadius.circular(10.0)),
+
+                    hintText: "Искать",
+                    hintStyle: const TextStyle(color: Color(0xFFFFFFFF), fontSize: 14.0),
+                    suffixIcon: const Icon(AssetsIcon.search, size: 16,color: Colors.white,),
+                  ),
+                  
+                  style: const TextStyle(color: Colors.white, height: 1.5, fontSize: 14.0),
+                ),
+              ),
+            )
+          ],
         ),
-      )
+      ),
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,5 @@
-import 'dart:ui';
-
 import 'package:flutter/material.dart';
-// import 'package:music/components/header.dart';
+import 'package:music/components/header.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,41 +7,9 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF0F0F0F),
-        title: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                "KI",
-                style: TextStyle(color: Colors.white, fontSize: 28.0),
-              ),
-              SizedBox(
-                width: 268,
-                height: 28,
-                child: TextField(
-                  textAlignVertical: TextAlignVertical.center,
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(horizontal: 18.0),
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(width: 1, color: Color(0xFF2B2B2B)),
-                      borderRadius: BorderRadius.circular(10.0)
-                    ),
-                    focusedBorder: OutlineInputBorder(
-                      borderSide: const BorderSide(width: 1, color: Color(0xFF4F4E4E)),
-                      borderRadius: BorderRadius.circular(10.0)
-                    ),
-                    hintText: "Искать",
-                    hintStyle: const TextStyle(color: Color(0xFF727272), fontSize: 14.0),
-                  ),
-                  style: const TextStyle(color: Colors.white),
-                ),
-              ),
-            ],
-          ),
-        ),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(kToolbarHeight),
+        child: Header(),
       ),
       body: Container(
         color: const Color(0xFF0F0F0F),
