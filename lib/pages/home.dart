61c7360b9d3deec89e10c0d3a2ebdd0e50c65c3b
Korrado Inganamorte <petristomin1812@gmail.com>
2024-03-24 10:20:11 +0300

    add header

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 // import 'package:music/components/header.dart';
 
@@ -9,15 +11,39 @@ class Home extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: const Color(0xFF0F0F0F),
-        actions: [
-          Container(
-            margin: const EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
-            child: const Text(
-              "KI",
-              style: TextStyle(color: Colors.white),
-            ),
-          )
-        ],
+        title: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Text(
+                "KI",
+                style: TextStyle(color: Colors.white, fontSize: 28.0),
+              ),
+              SizedBox(
+                width: 268,
+                height: 28,
+                child: TextField(
+                  textAlignVertical: TextAlignVertical.center,
+                  decoration: InputDecoration(
+                    contentPadding: const EdgeInsets.symmetric(horizontal: 18.0),
+                    enabledBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 1, color: Color(0xFF2B2B2B)),
+                      borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderSide: const BorderSide(width: 1, color: Color(0xFF4F4E4E)),
+                      borderRadius: BorderRadius.circular(10.0)
+                    ),
+                    hintText: "Искать",
+                    hintStyle: const TextStyle(color: Color(0xFF727272), fontSize: 14.0),
+                  ),
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
       body: Container(
         color: const Color(0xFF0F0F0F),
